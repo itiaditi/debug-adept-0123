b28a48848582950d12b60ae831f407b7c2ae7aa2
Indrani-Paul <paul.indrani999@gmail.com>
2024-02-25 23:22:13 +0530

    login functionality added

--- snapBasket/script/index.js
+++ snapBasket/script/index.js
@@ -1,3 +1,68 @@
+//login 
+  // login submit button
+  console.clear();
+  const loginModal = document.getElementById("loginModal");
+  const openModalButton = document.getElementById("openModal");
+  const loginBtn = document.getElementById("login");
+  const signupBtn = document.getElementById("signup");
+
+  function openModal() {
+    loginModal.style.display = "block";
+    document.getElementById("mainBody").style.filter = "blur(5px)";
+  }
+  
+
+  function closeModal() {
+    loginModal.style.display = "none";
+    document.getElementById("mainBody").style.filter = "blur(0px)";
+  }
+
+  openModalButton.addEventListener("click", openModal);
+  openModalButton.addEventListener("dblclick", closeModal);
+  // Close the modal when clicking outside of it
+  window.addEventListener("click", function (event) {
+    if (event.target === loginModal) {
+      closeModal();
+    }
+  });
+
+  // Close the modal when pressing the ESC key
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  loginBtn.addEventListener("click", (e) => {
+    let parent = e.target.parentNode.parentNode;
+    Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+      if (element !== "slide-up") {
+        parent.classList.add("slide-up");
+      } else {
+        signupBtn.parentNode.classList.add("slide-up");
+        parent.classList.remove("slide-up");
+      }
+    });
+  });
+
+  signupBtn.addEventListener("click", (e) => {
+    let parent = e.target.parentNode;
+    Array.from(e.target.parentNode.classList).find((element) => {
+      if (element !== "slide-up") {
+        parent.classList.add("slide-up");
+      } else {
+        loginBtn.parentNode.parentNode.classList.add("slide-up");
+        parent.classList.remove("slide-up");
+      }
+    });
+  });
+// ***************************************************************************
+
+
+
+
+
+
 //cart click 
 let cartClick=document.getElementById('cart');
 cartClick.addEventListener('click',()=>{
